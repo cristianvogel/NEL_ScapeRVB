7b3c0965f6fa99ad281a83596f9a507dc5da1826
Cristian Vogel <cristian.vogel55@gmail.com>
2024-08-01 18:38:58 +0200

    Corrected dynamic submodule linking

--- scripts/build-native.mjs
+++ scripts/build-native.mjs
@@ -20,14 +20,14 @@ let devFlag = argv.dev ? '-DELEM_DEV_LOCALHOST=1' : '';
 if (os.platform() === 'darwin') {
     // macOS specific code
     // uncomment this to make the stuff in build/scripted runnable in XCode
-    await $`cmake -G Xcode -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES="arm64" ${devFlag} ../..`;
-   // await $`cmake -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES="arm64" ${devFlag} ../..`;
-   
+    // await $`cmake -G Xcode -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES="arm64" ${devFlag} ../..`;
+
+    await $`cmake -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES="arm64" ${devFlag} ../..`;
     await $`cmake --build . --config ${buildType} -j 4`;
 
     // comment out not to build for x86_64 
-  //  await $`cmake -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_OSX_ARCHITECTURES="x86_64" ${devFlag} ../..`;
-   // await $`cmake --build . --config ${buildType} -j 4`;
+    //  await $`cmake -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_OSX_ARCHITECTURES="x86_64" ${devFlag} ../..`;
+    // await $`cmake --build . --config ${buildType} -j 4`;
 
 } else if (os.platform() === 'win32') {     //nodejs os returns win32 even on 64-bit Windows.
     await $`cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ ${devFlag} ../..`;
