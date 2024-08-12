78c938e6f46d83385276504a91a3a6ea053b2968
Cristian Vogel <cristian.vogel55@gmail.com>
2024-08-12 11:43:02 +0200

    implemented audio rate structure array

--- dsp/main.ts
+++ dsp/main.ts
@@ -1,12 +1,18 @@
 //@ts-check
 
 
-import { Renderer, el, createNode } from '@elemaudio/core';
+import { Renderer, el, createNode, ElemNode } from '@elemaudio/core';
+import { argMax, rotate } from "@thi.ng/arrays";
 import { RefMap } from './RefMap';
 import srvbEarly from './srvb-er';
 import { clamp, smoothStep, schlick, EPS } from '@thi.ng/math';
-import { NUM_SEQUENCES } from './srvb-er';
+import { equiv } from '@thi.ng/equiv';
+import { NUM_SEQUENCES, OEIS_SEQUENCES } from './srvb-er';
 
+type StructureData = {
+  consts: Array<ElemNode>;
+  max: () => ElemNode;
+};
 
 // First, we initialize a custom Renderer instance that marshals our instruction
 // batches through the __postNativeMessage__ function to direct the underlying native
@@ -17,41 +23,50 @@ let core = new Renderer((batch) => {
 });
 
 // Next, a RefMap for coordinating our refs
-let refs = new RefMap(core);
+let refs:RefMap = new RefMap(core);
 
 // Create our custom nodes
 // let _convolver = (props, ...childs) => createNode("convolver", props, childs);
 
-let __memState = null;
-let t = 0.0; // interpolation time for size param
 
 // the conditions that will trigger a full re-render of the node graph
 function shouldRender(prev, curr) {
   const result = (prev === null)
     || (curr === null)
     || (prev.sampleRate !== curr.sampleRate)
-    || (prev.structure !== Math.round(curr.structure * NUM_SEQUENCES) )
+    || (prev.structure !== Math.round(curr.structure * NUM_SEQUENCES))
   return result;
 }
 
-globalThis.__receiveStateChange__ = ( stateReceivedFromNative ) => {
+let structureData:StructureData = {
+  consts: convertSeriesToConsts(OEIS_SEQUENCES[0].slice(0, 8), refs),
+  max: ()=>el.const({ value: 0 }),
+};
 
-  const __state = JSON.parse( stateReceivedFromNative );
+
+// using memoization to store the last state and only re-render if the state has changed
+let __memState: null | any = null;
+let t = 0.0; // interpolation time for size param
+
+globalThis.__receiveStateChange__ = (stateReceivedFromNative) => {
+
+  const __state = JSON.parse(stateReceivedFromNative);
 
   smoothSizeInterpolation();
 
   // interpreted state, any adjustments should be done here before rendering to the graph
   const srvb = {
-    size:  __state.size ,
-    dimension: clamp( __state.dimension, EPS, 1 - EPS ), 
+    size: __state.size,
+    dimension: clamp(__state.dimension, EPS, 1 - EPS),
     decay: __state.decay,
     mix: __state.mix,
-    tone: clamp(__state.tone * 2 - 1, -0.99, 1) ,
-    structure: Math.round( __state.structure * NUM_SEQUENCES ),
+    tone: clamp(__state.tone * 2 - 1, -0.99, 1),
+    structure: Math.round(__state.structure * NUM_SEQUENCES),
+    structureMax: __state.structureMax,
   };
 
-
-  if (shouldRender(__memState, __state) ) {
+  if (shouldRender(__memState, __state)) {
+     structureData = handleStructureChange( refs, srvb.structure );
     const graph = core.render(...srvbEarly({
       key: 'srvbEarly',
       sampleRate: __state.sampleRate,
@@ -61,8 +76,11 @@ globalThis.__receiveStateChange__ = ( stateReceivedFromNative ) => {
       tone: refs.getOrCreate('tone', 'const', { value: srvb.tone }, []), // coming always as 0-1
       dimension: refs.getOrCreate('dimension', 'const', { value: srvb.dimension }, []), // coming always as 0-1
       structure: srvb.structure || 0,
+      structureMax: structureData.max(),
+      
     },
-      el.in({ channel: 0 }), el.in({ channel: 1 }))
+      [ el.in({ channel: 0 }), el.in({ channel: 1 }) ],
+      ...structureData.consts)
     );
   } else {
 
@@ -71,8 +89,17 @@ globalThis.__receiveStateChange__ = ( stateReceivedFromNative ) => {
     refs.update('mix', { value: srvb.mix });
     refs.update('tone', { value: srvb.tone });
     refs.update('dimension', { value: srvb.dimension });
+    refs.update('structureMax', { value: srvb.structureMax });
+
+   // update the structure consts
+    OEIS_SEQUENCES[srvb.structure].forEach((value, i) => {
+      refs.update(`structureConstNode_${i}`, { value });
+    });
+
+
+
   }
- 
+
   __memState = __state;
   __memState.structure = srvb.structure;
 
@@ -95,7 +122,41 @@ globalThis.__receiveStateChange__ = ( stateReceivedFromNative ) => {
       interpTimer(__memState.size, __state.size);
     }
   }
-};
+
+
+}; // end of receiveStateChange
+
+  // build the structure outside the dsp code
+  function handleStructureChange( _refs:RefMap, currStructIndex = 0) {
+    {
+      console.log( OEIS_SEQUENCES );
+      let series = OEIS_SEQUENCES[currStructIndex].slice(0, 8);
+      console.log(`Using series ${series} `);
+      // this should compute norm only on the set actually being used which is 8 at a time
+      const seriesMax = Math.min( series[argMax(series, 17)], 137 );
+      // as the overall volume of a delayunit is also computed from the series, i had to put an upped limit
+      // on the max value to prevent the series from being too quiet ( i.e. the normalised range is too wide )
+      console.log(`Using series max ${seriesMax} `);
+      // convert the sequences to signals
+  
+      const sequenceAsSignals = convertSeriesToConsts(series, _refs);
+
+      const structureEight:StructureData = {
+        consts: sequenceAsSignals,
+        max: ()=>_refs.getOrCreate( 'structureMax', 'const', { value: seriesMax, key: `oeis:max_${series.slice(0,8)}` }, [] ),
+      };
+
+      return structureEight;
+    };
+  }
+
+function convertSeriesToConsts(series: number[], _refs: RefMap) {
+  return series.map((value, j) => {
+    const t = _refs.getOrCreate(`structureConstNode_${j}`, "const", { value, key: `structureConst:${j}` }, []);
+    return t;
+  });
+}
+
 
 /////////////////////////////////////////////////////////////////
 // Finally, an error callback which just logs back to native
@@ -121,3 +182,5 @@ globalThis.__receiveHydrationData__ = (data) => {
     });
   }
 };
+
+
--- dsp/srvb-er.ts
+++ dsp/srvb-er.ts
@@ -6,25 +6,26 @@ import { Smush32 } from "@thi.ng/random";
 
 // THese number seies are from the OEIS and all sound really cool
 const smush = new Smush32(0xcafebabe);
-const oeisSequences = [
+export const OEIS_SEQUENCES = [
   [39, 31, 23, 17, 34, 68, 76, 63, 50, 37, 28, 20, 55, 110, 123, 102], // A035506		Stolarsky array read by antidiagonals.  ⭐️⭐️⭐️⭐️
   [16, 22, 31, 40, 52, 68, 90, 121, 152, 192, 244, 312, 402, 523, 644, 796], //  A007604		a(n) = a(n-1) + a(n-1-(number of odd terms so far))
   [22, 37, 21, 59, 27, 43, 24, 83, 35, 53, 26, 31, 20, 29, 18, 67], // A227413		a(1)=1, a(2n)=nthprime(a(n)), a(2n+1)=nthcomposite(a(n)), where nthprime = A000040, nthcomposite = A002808.
   [17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79], // A000040		The prime numbers: a(n) = n-th prime.
   [17, 6, 33, 10, 65, 7, 129, 18, 11, 257, 513, 34, 1025, 13, 19, 66], // 		Inverse of sequence A052330 considered as a permutation of the natural numbers.
-  [17, 19, 23, 26, 30, 35, 40, 46, 52, 60, 67, 77, 87, 98, 111, 124 ],  // A000607		Number of partitions of n into prime parts.
+  [17, 19, 23, 26, 30, 35, 40, 46, 52, 60, 67, 77, 87, 98, 111, 124], // A000607		Number of partitions of n into prime parts.
   [13, 18, 23, 30, 37, 47, 57, 70, 84, 101, 119, 141, 164, 192, 221, 255], // A01401 Number of partitions of n into at most 5 parts.
   [34, 47, 62, 79, 97, 118, 141, 166, 192, 221, 253, 285, 320, 357, 395, 436], // A064356		Dimension of J_(12n,n+1), the Jacobi form of weight 12n and index n+1.
-  [ 38, 67, 143, 232, 10, 22, 46, 101, 177, 376, 609], // A0355077 	Inverse Stolarsky array read by antidiagonals  :: cropped down
-  [15, 18, 22, 27, 32, 38, 46, 54, 64, 76, 89, 104, 122, 142, 165, 192 ], // A000009 number of partitions of n into distinct parts; number of partitions of n into odd parts.
+  [38, 67, 143, 232, 10, 22, 46, 101, 177, 376, 609], // A0355077 	Inverse Stolarsky array read by antidiagonals  :: cropped down
+  [15, 18, 22, 27, 32, 38, 46, 54, 64, 76, 89, 104, 122, 142, 165, 192], // A000009 number of partitions of n into distinct parts; number of partitions of n into odd parts.
   [97, 66, 53, 35, 26, 144, 322, 267, 301, 212, 157, 107, 86, 57, 43, 28], // A199535		Clark Kimberling's even first column Stolarsky array read by antidiagonals.  quite large
-  [16, 42, 20, 32, 24, 52, 18, 40, 24, 36, 28, 58, 16, 60, 30, 36],  // A000010		Euler totient function phi(n)
-  [17, 34, 13, 28, 25, 46, 75, 42, 69, 104, 37, 62, 95, 58, 55, 86 ], // A316667		Squares visited by a knight moving on a spirally numbered board always to the lowest available unvisited square.
-  [ 17, 21, 26, 31, 37, 45, 53, 63, 75, 88, 103, 121, 141, 164, 191, 221], // A111133		Number of partitions of n into at least two distinct parts.
-  [ 16, 22, 29, 37, 46, 56, 67, 79, 92, 106, 121, 137, 154, 172, 191, 211 ], // A000124		Central polygonal numbers (the Lazy Caterer's sequence)
-  [ 17, 43, 16, 44, 15, 45, 14, 46, 79, 113, 78, 114, 77, 39, 78, 38 ] // A005132		Recamán's sequence 
+  [16, 42, 20, 32, 24, 52, 18, 40, 24, 36, 28, 58, 16, 60, 30, 36], // A000010		Euler totient function phi(n)
+  [17, 34, 13, 28, 25, 46, 75, 42, 69, 104, 37, 62, 95, 58, 55, 86], // A316667		Squares visited by a knight moving on a spirally numbered board always to the lowest available unvisited square.
+  [17, 21, 26, 31, 37, 45, 53, 63, 75, 88, 103, 121, 141, 164, 191, 221], // A111133		Number of partitions of n into at least two distinct parts.
+  [16, 22, 29, 37, 46, 56, 67, 79, 92, 106, 121, 137, 154, 172, 191, 211], // A000124		Central polygonal numbers (the Lazy Caterer's sequence)
+  [17, 43, 16, 44, 15, 45, 14, 46, 79, 113, 78, 114, 77, 39, 78, 38], // A005132		Recamán's sequence
 ];
-export const NUM_SEQUENCES = oeisSequences.length - 1;
+
+export const NUM_SEQUENCES = OEIS_SEQUENCES.length - 1;
 
 const seededNormMinMax: Array<number> = Array.from({ length: 8 }).map(() =>
   smush.normMinMax(0.01, 1.618)
@@ -53,44 +54,43 @@ const H8 = [
 
 // A diffusion step expecting exactly 8 input channels with
 // a maximum diffusion time of 500ms
-type Sequence = {
-  integers: Array<number>;
+type Structure = {
+  integers: Array<number> | Array<ElemNode>;
   length: () => number;
-  max: number;
+  max: number | ElemNode;
 };
 
 type DiffuseProps = {
   seededNormMinMax?: number;
-  sequence: Sequence;
+  structure: Array<ElemNode>;
   maxLengthSamp: number;
 };
 
 function diffuse(props: DiffuseProps, ...ins) {
-  const { maxLengthSamp: size } = props;
-  const { sequence } = props;
-  const structure = sequence.integers;
-  const structureMax = sequence.max;
+  const { maxLengthSamp } = props;
+  const structure: Array<ElemNode> = props.structure;
+ // const structureMax: ElemNode = props.structure.max;
   const len = ins.length; // 8
 
   // keep this one uniform?
-  const diffusionStageLevel = (i: number) =>  { 
-    const baseAtt = Math.sqrt(1 / (structure[i] / len) );
-    return baseAtt }; 
+  const diffusionStageLevel = (i: number): number => {
+    const baseAtt = Math.sqrt(1 / len);
+    return baseAtt;
+  };
 
   const dels = ins.map(function (input, i) {
-    const lineSize = size; // perceptual scaling
-
     return el.delay(
-      { size: lineSize, key: `srvb-diff:${i}` },
-      1, //more randomisation seededNormMinMax[i],
+      { size: maxLengthSamp, key: `srvb-diff:${i}` },
+    //  el.div( structure[i], structureMax ),
+    structure[i % structure.length] , 
       0,
-     input
+      input
     );
   });
   return H8.map(function (row, i) {
     return el.add(
       ...row.map(function (col, j) {
-        return el.mul(col * diffusionStageLevel(i), dels[j]);
+        return el.mul( col , diffusionStageLevel(i), dels[j]);
       })
     );
   });
@@ -100,7 +100,8 @@ function diffuse(props: DiffuseProps, ...ins) {
 type FDNProps = {
   name: string;
   sampleRate: number;
-  sequence: Sequence;
+  structureArray: Array<ElemNode>;
+  structureMax: ElemNode;
   tone: ElemNode;
   size: ElemNode;
   decay: ElemNode;
@@ -109,36 +110,39 @@ type FDNProps = {
 
 function dampFDN(props: FDNProps, ...ins) {
   const len = ins.length;
-  const { name, sequence, tone, size, decay, modDepth } = props;
+  const { name, tone, size, decay, modDepth } = props;
   const { sampleRate } = props;
-  const structure = sequence.integers;
-  const structureMax = sequence.max;
+  const structure: Array<ElemNode> = props.structureArray;
+  const structureMax: ElemNode = props.structureMax;
 
   /**
-   * 
-   * normalised scale the level of the taps according 
-   * to the structure. Shortest, loudest... 
-   * 
+   *
+   * normalised scale the level of the taps according
+   * to the structure. Shortest, loudest...
+   *
    */
-  const tapDelayLevel = (i: number) =>  { 
+  const tapDelayLevel = (i: number) => {
     const baseAtt = Math.sqrt(1 / len);
-    const normStruct = ( 1 - (structure[i] % structure.length) / structureMax );  
-  //  console.log ('tapLevel: ' , normStruct * baseAtt )
-    return normStruct * baseAtt }; 
- // const tapDelayLevel = ()=> Math.sqrt(1 / len);
-  const md = modDepth;
+    const normStruct = el.sub(
+      1.0, // maybe should be more than 1
+      el.div( structure[i % structure.length], structureMax)  // todo: pass in the const of the max value
+    );
+    return el.mul(normStruct, baseAtt);
+  };
+  // const tapDelayLevel = ()=> Math.sqrt(1 / len);
+  //const md = modDepth;
 
-  const toneDial = (input, offset: number) => {
-    const dial = el.smooth(el.tau2pole(0.5), el.le(tone, 0));
-    const fcLPF = el.add(48, el.mul(12000 + offset, el.sub(1, el.abs(tone) ) ) );
+  const toneDial = (input, offset: ElemNode) => {
+    const dial = el.smooth( el.tau2pole(0.5), el.le(tone, 0));
+    const fcLPF = el.add(48, el.mul( el.add( 12000 , offset ), el.sub(1, el.abs(tone))));
     return el.select(
       dial,
       // darker
-      el.svf(fcLPF, 1 / offset, el.mul( el.db2gain(0.5) , input)),
+      el.svf(fcLPF, el.div(1 , offset), el.mul(el.db2gain(0.5), input)),
       // brighter
       el.svfshelf(
         { mode: "highshelf" },
-        650 + offset,
+        el.add( 650 , offset ),
         0.5,
         el.mul(tone, el.db2gain(6)),
         el.mul(el.db2gain(-1), input)
@@ -151,39 +155,35 @@ function dampFDN(props: FDNProps, ...ins) {
   const dels = ins.map(function (input, i) {
     return el.add(
       toneDial(input, structure[i]),
-      el.mul( decay, el.smooth(0.0105, el.tapIn({ name: `${name}:fdn${i}` })))
+      el.mul(decay, el.smooth(0.0105, el.tapIn({ name: `${name}:fdn${i}` })))
     );
   });
 
   let mix = H8.map(function (row, i) {
     return el.add(
       ...row.map(function (col, j) {
-        return el.mul(col * tapDelayLevel(i), dels[j]);
+        return el.mul( col, tapDelayLevel(i), dels[j]);
       })
     );
   });
 
-  return mix.map(function ( mm, i ) {
+  return mix.map(function (mm, i) {
     const ms2samps = (ms) => sampleRate * (ms / 1000.0);
 
     const delayScale = el.mul(
-      el.add( 1.0, el.sm(size) ),
-      ms2samps( structure[i] % structure.length )
+      el.add(1.0, el.sm(size)),
+     el.ms2samps( structure[i % structure.length])
     );
 
     return el.tapOut(
       { name: `${name}:fdn${i}` },
-      el.delay(
-       { size: ms2samps( structure[i] ) }, 
-        delayScale,
-        0.0,
-        mm 
-      ) ) }
-    )
-  }; // end of dampFDN
+      el.delay({ size: ms2samps(750) }, delayScale, 0.0, mm)
+    );
+  });
+} // end of dampFDN
 /** /
  * /// MAIN
-**/
+ **/
 interface SRVBProps {
   size: ElemNode;
   decay: ElemNode;
@@ -191,59 +191,55 @@ interface SRVBProps {
   mix: ElemNode;
   tone: ElemNode;
   dimension: ElemNode; // rounded integer behaviour
+  structureMax: ElemNode; // max value of the series
   // non-signal data
   sampleRate: number;
   structure: number;
   key: string;
 }
 
-export default function srvbEarly(props: SRVBProps, xl: ElemNode, xr: ElemNode ) {
-
+export default function srvbEarly(
+  props: SRVBProps,
+  inputs,
+  ...structureArray
+) {
   // xl , xr -- unprocessed input
-
-  const { sampleRate, key, dimension, structure } = props;
-
+  const { sampleRate, key, dimension, structureMax } = props;
+  const  [ xl, xr ] = inputs;
   // input attenuation
-  const _xl = el.dcblock(el.mul( xl ,el.db2gain(-1.5) ));
-  const _xr = el.dcblock(el.mul( xl, el.db2gain(-1.5) )) ;
-
-  
-  let series = oeisSequences[structure || 0].slice(0, 8);
-  console.log(`Using series ${series} `);
-   // this should compute norm only on the set actually being used which is 8 at a time
-  const seriesMax = Math.min( series[ argMax(series, 17) ] , 137 ); 
-  // as the overall volume of a delayunit is also computed from the series, i had to put an upped limit 
-  // on the max value to prevent the series from being too quiet ( i.e. the normalised range is too wide )
-  console.log(`Using series max ${seriesMax} ` )
-
-  const sequence = {
-    integers: series,
-    max: seriesMax,
-    length: () => series.length,
-  };
-  // normalise series using seriesMax
-
+  const _xl = el.dcblock(el.mul(xl, el.db2gain(-1.5)));
+  const _xr = el.dcblock(el.mul(xl, el.db2gain(-1.5)));
   // Upmix to eight channels
   const mid = el.mul(0.5, el.add(_xl, _xr));
   const side = el.mul(0.5, el.sub(_xl, _xr));
   const four = [_xl, _xr, mid, side];
   const eight = [...four, ...four.map((x) => el.mul(-1, x))];
-
   // Diffusion over 8 channels using core sequence for coefficients
   const ms2samps = (ms) => sampleRate * (ms / 1000.0);
-  const d1 = diffuse({ sequence, maxLengthSamp: ms2samps(49) }, ...eight);
-  const d2 = diffuse({ sequence, maxLengthSamp: ms2samps(363) }, ...d1);
-  const d3 = diffuse({ sequence, maxLengthSamp: ms2samps(137) }, ...d2);
+
+  const d1 = diffuse(
+    { structure: structureArray, maxLengthSamp: ms2samps(43) },
+    ...eight
+  );
+  const d2 = diffuse(
+    { structure: structureArray, maxLengthSamp: ms2samps(97) },
+    ...d1
+  );
+  const d3 = diffuse(
+    { structure: structureArray, maxLengthSamp: ms2samps(117) },
+    ...d2
+  );
 
   // Reverb network
   const d4: ElemNode[] = dampFDN(
     {
       name: `${key}:d4`,
       sampleRate,
-      sequence,
+      structureArray,
       tone: props.tone,
       size: props.size,
       decay: 0.004,
+      structureMax
     },
     ...d3
   );
@@ -251,32 +247,33 @@ export default function srvbEarly(props: SRVBProps, xl: ElemNode, xr: ElemNode )
     {
       name: `${key}:r0`,
       sampleRate,
-      sequence,
+      structureArray,
       tone: props.tone,
       size: props.size,
       decay: props.decay,
+      structureMax
     },
     ...d4
   );
 
   // interleaved dimensional Downmix ( optimised to build the spatial delays when needed )
-  let spat = ( i, x: ElemNode ): ElemNode => el.delay( { key: `downmix:${i}`, size: ms2samps( series[ 7 - i ]) }, el.sm( el.sub(1, dimension) ), 0, x);
+  let spat = (i, x: ElemNode): ElemNode =>
+    el.delay(
+      { key: `downmix:${i}`, size: ms2samps(100) },
+      el.sm(el.sub(1, dimension)),
+      0,
+      x
+    );
 
-  let yl = el.mul(el.db2gain(-3), 
-                      el.add(
-                            spat( 0, r0[0] ), 
-                            r0[1], 
-                            spat( 2, r0[2] ),
-                            r0[3]
-                          ));
+  let yl = el.mul(
+    el.db2gain(-3),
+    el.add(spat(0, r0[0]), r0[1], spat(2, r0[2]), r0[3])
+  );
 
-  let yr = el.mul(el.db2gain(-3),
-                      el.add(
-                            r0[4], 
-                            spat( 5, r0[5] ), 
-                            r0[6], 
-                            spat( 7, r0[7] )
-                          ));
+  let yr = el.mul(
+    el.db2gain(-3),
+    el.add(r0[4], spat(5, r0[5]), r0[6], spat(7, r0[7]))
+  );
   // Wet dry mixing
   return [el.select(props.mix, yl, xl), el.select(props.mix, yr, xr)];
 }
