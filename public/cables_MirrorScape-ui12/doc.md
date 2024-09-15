ee2b4d3cc25b13512a84d794657d33277598734d
Cristian Vogel <cristian.vogel55@gmail.com>
2024-09-15 15:30:22 +0200

    DEbugging view initialisation

---
+++ public/cables_MirrorScape-ui12/doc.md
@@ -0,0 +1,36 @@
+## Patch Variables:
+
+* ____bodyHitY__ ```Number```
+* ____currentReflectorParam__ ```String```
+* ____heightMapGeometry__ ```Object```
+* ____labelBoolAnim__ ```Number```
+* ____lensFlareTexts__ ```Texture```
+* ___currentDialValueInsideRepeat2D__ ```Number```
+* ___currentIndexInsideRepeat2D__ ```Number```
+* ___currentInstanceIsActive__ ```Number```
+* ___finalTextOut__ ```Texture```
+* ___rep2d_x__ ```Number```
+* __ext_additionalParams_object__ ```Object```
+* __ext_changingParamID__ ```String``` (default Value: `New String`)
+* __ext_normValue__ ```Number``` (default Value: `0`)
+* __ext_srvbParams_object__ ```Object```
+* __NEL_SRVB_Metals__ ```Object```
+* __ringG__ ```Number```
+* __ringR__ ```Number```
+* __ui_activeDial_ID__ ```Number```
+* __ui_additionalParams_object__ ```Object```
+* __ui_dialValues_array__ ```Array```
+* __ui_hitBodyName__ ```String```
+* __ui_leftButtonIsDown__ ```Number``` (default Value: `0`)
+* __ui_mouseIsChangingDialValue__ ```Number```
+
+  detect interaction with the GL dials
+
+* __ui_mouseIsChangingParamID__ ```String``` (default Value: `New String`)
+* __ui_mouseOverParamID__ ```String```
+* __ui_normValue__ ```Number```
+* __ui_smoothedParam_diffusion__ ```Number```
+* __ui_smoothedParam_mix__ ```Number```
+* __ui_smoothedParam_size__ ```Number```
+* __ui_smoothedParam_tone__ ```Number```
+
---
+++ public/cables_MirrorScape-ui12/ops/patches/Ops.Patch.P4Zknbo/Ops.Patch.P4Zknbo.BasicSlider_v2/att_inc_gen_ports.js
@@ -0,0 +1,49 @@
+const port_blls2amps=op.inTrigger("blls2amps");
+port_blls2amps.setUiAttribs({title:"Trigger in",});
+
+const port_btagbs7z2=op.inString("btagbs7z2","");
+port_btagbs7z2.setUiAttribs({title:"Class",});
+
+const port_lj06d561p=op.inFloat("lj06d561p",0);
+port_lj06d561p.setUiAttribs({title:"Show Boundings",display:"bool",});
+
+const port_nti406vwi=op.outNumber("nti406vwi");
+port_nti406vwi.setUiAttribs({title:"Result",});
+
+const port_gjvx0zyv5=op.outNumber("gjvx0zyv5");
+port_gjvx0zyv5.setUiAttribs({title:"value",});
+
+op.initInnerPorts=function(addedOps)
+{
+  for(let i=0;i<addedOps.length;i++)
+  {
+    if(addedOps[i].innerInput)
+    {
+const innerOut_blls2amps = addedOps[i].outTrigger("innerOut_blls2amps");
+innerOut_blls2amps.setUiAttribs({title:"Trigger in"});
+port_blls2amps.onTriggered = () => { innerOut_blls2amps.trigger(); };
+
+const innerOut_btagbs7z2 = addedOps[i].outString("innerOut_btagbs7z2");
+innerOut_btagbs7z2.set(port_btagbs7z2.get() );
+innerOut_btagbs7z2.setUiAttribs({title:"Class"});
+port_btagbs7z2.on("change", (a,v) => { innerOut_btagbs7z2.set(a); });
+
+const innerOut_lj06d561p = addedOps[i].outNumber("innerOut_lj06d561p");
+innerOut_lj06d561p.set(port_lj06d561p.get() );
+innerOut_lj06d561p.setUiAttribs({title:"Show Boundings"});
+port_lj06d561p.on("change", (a,v) => { innerOut_lj06d561p.set(a); });
+
+    }
+if(addedOps[i].innerOutput)
+{
+const innerIn_nti406vwi = addedOps[i].inFloat("innerIn_nti406vwi");
+innerIn_nti406vwi.setUiAttribs({title:"Result"});
+innerIn_nti406vwi.on("change", (a,v) => { port_nti406vwi.set(a); });
+
+const innerIn_gjvx0zyv5 = addedOps[i].inFloat("innerIn_gjvx0zyv5");
+innerIn_gjvx0zyv5.setUiAttribs({title:"value"});
+innerIn_gjvx0zyv5.on("change", (a,v) => { port_gjvx0zyv5.set(a); });
+
+}
+}
+};
