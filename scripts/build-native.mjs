a42d0517b2198094bcc76519a797246236f80942
Cristian Vogel <cristian.vogel55@gmail.com>
2024-10-29 20:12:53 +0100

    Clean

--- scripts/build-native.mjs
+++ scripts/build-native.mjs
@@ -21,12 +21,12 @@ let devFlag = argv.dev ? '-DELEM_DEV_LOCALHOST=1' : '';
 
 if (os.platform() === 'darwin') {
     // macOS specific code // -G Xcode for Xcode project
-      await $`cmake -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_TOOLCHAIN_FILE=${rootDir}/toolchain.cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=13 -DCMAKE_OSX_ARCHITECTURES="arm64" ${devFlag} ../..`;
+      await $`cmake -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/  -DCMAKE_OSX_DEPLOYMENT_TARGET=13 -DCMAKE_OSX_ARCHITECTURES="arm64" ${devFlag} ../..`;
       await $`cmake --build . --config ${buildType} -j 4`;
 
     // Uncomment the following lines if you need to build for x86_64 as well
-     await $`cmake  -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/ -DCMAKE_TOOLCHAIN_FILE=${rootDir}/toolchain.cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES="x86_64" ${devFlag} ../..`;
-     await $`cmake --build . --config ${buildType} -j 4`;
+    // await $`cmake  -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_INSTALL_PREFIX=./out/  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES="x86_64" ${devFlag} ../..`;
+    // await $`cmake --build . --config ${buildType} -j 4`;
 
 } else if (os.platform() === 'win32') {
     // Windows specific code
